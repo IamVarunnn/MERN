01e0e8c823e66b75d59d0e60742109854791df1a
IamVarunnn <varunrakesh16@gmail.com>
2025-11-09 20:15:48 +0530

    Learned about Date object

---
+++ JavaScript/index.js
@@ -0,0 +1,31 @@
+
+// const now = Date();
+// const dates = new Date();
+// console.log(dates);
+// console.log(dates.toString());
+
+
+// const now = new Date();
+
+// console.log(now);
+
+// console.log(now.toString());
+// console.log(now.toISOString());
+// console.log(now.toLocaleString());
+
+// const newDate = new Date(2025,8,11,12,40,122);
+// console.log(newDate);
+// console.log(newDate.toString());
+
+// const now = Date.now();
+// console.log(now);
+// // 1762699222431
+
+// const dates = new Date(1762699222431);
+// console.log(dates.toString());
+
+
+const now = Date.now();
+const dates = new Date(0);
+
+console.log(dates.toString());
