e9c44c787bf4efd4b72e94e30ab05afe3c8165b0
IamVarunnn <varunrakesh16@gmail.com>
2025-12-01 18:53:06 +0530

    Learned about event listeners and also about target

--- JavaScript/Day 15/script.js
+++ JavaScript/Day 15/script.js
@@ -1,5 +1,5 @@
 
-let heading = document.querySelector("h1");
+// let heading = document.querySelector("h1");
 
 // function handleClick(){
 //     heading.style.backgroundColor = "blue";
@@ -38,7 +38,7 @@ let heading = document.querySelector("h1");
 // });
 
 
-let parent = document.getElementById("parent");
+// let parent = document.getElementById("parent");
 
 // let child = document.getElementsByClassName("child");
 
@@ -56,8 +56,123 @@ let parent = document.getElementById("parent");
 
 
 
-for(let child of parent.children){
-    child.addEventListener("click", () => {
-        child.style.backgroundColor = "black";
-    });
+// for(let child of parent.children){
+//     child.addEventListener("click", () => {
+//         child.style.backgroundColor = "black";
+//     });
+// }
+
+
+
+// let grandparent = document.querySelector(".grandparent");
+// let parent = document.querySelector(".parent");
+// let child = document.querySelector(".child");
+
+
+
+//By default it is false
+// grandparent.addEventListener("click",()=>{
+ 
+//     console.log("Gp was clicked")
+// })
+
+// parent.addEventListener("click",()=>{
+    
+//     console.log("p was clicked")
+// })
+
+// child.addEventListener("click",()=>{
+    
+//     console.log("child was clicked")
+// })
+
+// child was clicked
+// p was clicked
+// Gp was clicked
+
+// grandparent.addEventListener("click",()=>{
+ 
+//     console.log("Gp was clicked")
+// },true)
+
+// parent.addEventListener("click",()=>{
+    
+//     console.log("p was clicked")
+// },true)
+
+// child.addEventListener("click",()=>{
+    
+//     console.log("child was clicked")
+// },true)
+
+// Gp was clicked
+// p was clicked
+// child was clicked
+
+
+//Capture phase : 
+// Its like visited array we use in the DSA like we
+// would travserse the
+// tree from top to down and see
+// wethere that was visited or not if its true then 
+// execute if not then just traverse and come back.
+
+
+
+// const heading = document.querySelector("h1");
+
+// heading.addEventListener("click",(e)=>{
+//     console.log(e);
+// })
+
+// This e bring all the information about the object
+// we are currently lisntening like type of event cliked etc
+
+//If we want to stop its default execution like event bubbling we use like
+// e.stopPropgation();
+
+//The e is so powerful we can see who triggered that event like
+// e.target
+
+
+let parent = document.getElementById("parent");
+
+// let child = document.getElementsByClassName("child");
+// parent.addEventListener("click",(e)=>{
+//     console.log(e.target);
+//     e.target.textContent = "iam cliked"
+// })
+
+// child.addEventListener("click",()=>{
+//     for(let children of child){
+//         children.style.backgroundColor = "black";
+//     }
+// })
+
+// for (let box of child) {
+//   box.addEventListener("click", () => {
+//     box.style.backgroundColor = "black";
+//   });
+// }
+
+
+//If we want to remove event listener
+
+// function handleClick(e){
+//     e.target.textContent = "Iam cliked"
+// }
+
+// parent.removeEventListener("click",handleClick);
+// parent.addEventListener("click",handleClick);
+
+
+//If we want at least once then use the below
+
+
+function handleClick(e){
+    e.target.textContent = "Iam cliked";
+    parent.removeEventListener("click",handleClick);
 }
+
+parent.addEventListener("click",handleClick);
+
